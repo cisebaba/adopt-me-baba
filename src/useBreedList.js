5f9553536fdd75b0bcbe787ff044e6c052b3b32f
cisebaba <cisebabatasi@gmail.com>
2022-11-15 11:20:07 -0800

    hello world

---
+++ src/useBreedList.js
@@ -0,0 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
+
+export default function useBreedList(animal) {
+  const results = useQuery(["breeds", animal], fetchBreedList);
+
+  return [results?.data?.breeds ?? [], results.status];
+}
+// import { useState, useEffect } from "react";
+
+// const localChache = {};
+
+// export default function useBreedList(animal) {
+//   const [breedList, setBreedList] = useState([]);
+//   const [status, setStatus] = useState("unloaded");
+
+//   useEffect(() => {
+//     if (!animal) {
+//       setBreedList([]);
+//     } else if (localChache[animal]) {
+//       setBreedList(localChache[animal]);
+//     } else {
+//       requestBreedList();
+//     }
+//     async function requestBreedList() {
+//       setBreedList([]);
+//       setStatus("loading");
+//       const res = await fetch(
+//         `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+//       );
+//       const json = await res.json();
+//       localChache[animal] = json.breeds || [];
+//       setBreedList(localChache[animal]);
+//       setStatus("loaded");
+//     }
+//   }, [animal]);
+//   return [breedList, status];
+// }
